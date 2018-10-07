36c20330c8c3c9e147ad3d65033d58d9862733f5
Christian Brüggemann <mail@cbruegg.com>
2018-10-07 18:34:16 +0200

    Remove star import

--- src/main/java/com/jakewharton/retrofit2/adapter/kotlin/coroutines/CoroutineCallAdapterFactory.kt
+++ src/main/java/com/jakewharton/retrofit2/adapter/kotlin/coroutines/CoroutineCallAdapterFactory.kt
@@ -17,7 +17,12 @@ package com.jakewharton.retrofit2.adapter.kotlin.coroutines
 
 import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.Deferred
-import retrofit2.*
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import retrofit2.Retrofit
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
 
